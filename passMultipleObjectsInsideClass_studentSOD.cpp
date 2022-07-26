40d942fa5037dbeacef96d47e3bc1be311261ce4
Shubham <shubhamprasad773@gmail.com>
2022-07-26 22:00:53 +0530

    26 july

--- passMultipleObjectsInsideClass_studentSOD.cpp
+++ passMultipleObjectsInsideClass_studentSOD.cpp
@@ -1,3 +1,8 @@
+// wap to calculate sum of SOD of marks of student and print the name and sum of SOD of marks of that student
+// for example : 
+    // input : 11 22 33 44 55
+    // sod of input : 1+1+2+2++3+3+4+4+5+5 = 30
+
 #include <iostream>     //SOD : Sum of Digit
 using namespace std;
 class student{
@@ -6,7 +11,7 @@ class student{
         string name;
     public:
         void input(){
-            cout<<"enter name : ";
+            cout<<"\nenter name : ";
             cin>>name;
             for(int i=0;i<5;i++){
                 cout<<"enter marks : ";
@@ -15,6 +20,31 @@ class student{
         }
         static void SOD(student *s,int n){
             
+            int sum[n]={0,0,0,0,0};
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    while(s[i].marks[j]!=0)
+                    {
+                        int temp=s[i].marks[j]%10;
+                        sum[i]+=temp;
+                        s[i].marks[j]/=10;
+                    }
+                }
+            }
+            int max=sum[0],location;
+            for(int i=0;i<n;i++)
+            {
+                if(max<sum[i])
+                {
+                    max=sum[i];
+                    location=i;
+                }
+            }
+            cout<<"\n--------Result---------\n";
+            cout<<"NAME : "<<s[location].name<<endl;
+            cout<<"SOD : "<<sum[location]<<endl;    //SOD :sum of digit
         }
 };
 main(){
@@ -22,7 +52,8 @@ main(){
     int n;
     cin>>n;
     student s[n];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         s[i].input();
     }
     student::SOD(s,n);
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-using namespace std;
-main(){
-    int a,b,c,cnt=1;
-    a=0,b=1;
-    for(cnt=1;cnt<=20;cnt++){
-        cout<<a<<"  ";
-        c=a+b;
-        a=b;
-        b=c;
-    }
-
-}
+sum[i]
